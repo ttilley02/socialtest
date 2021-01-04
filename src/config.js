929e01a28699f59ec4755820385713b0727b9fe0
ttilley02 <ttilley02@gmail.com>
2021-01-03 23:27:24 -0500

    first

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 
 
-module.exports = {
+  module.exports = {
    PORT: process.env.PORT || 8080,
-   NODE_ENV: process.env.NODE_ENV || "development",
-   DATABASE_URL:
-      process.env.DATABASE_URL || "postgresql://postgres@localhost/post-auth",
-   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
-};
+   NODE_ENV: process.env.NODE_ENV || 'development',
+   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/socialBack',
+   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/socialBack-test'
+ }
+
+
