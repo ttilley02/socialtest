7e13ada630a4ff8c8c384b1bb9b4c476735c2c97
ttilley02 <ttilley02@gmail.com>
2021-01-04 00:52:17 -0500

    auth fixed

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 
-
   module.exports = {
    PORT: process.env.PORT || 8080,
    NODE_ENV: process.env.NODE_ENV || 'development',
    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/socialBack',
-   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/socialBack-test'
+   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/socialBack-test',
+   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
  }
 
 
